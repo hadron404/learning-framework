73d654907881b5addb37e815ec4261655eb88b6a
zhouqiang <2902511578@qq.com>
2024-02-02 17:41:02 +0800

    方案示例：基于spring以及mybatis-plus的多租户数据库隔离-共享表模型方案实现

--- mybatis-plus/multitenant/partition/build.gradle.kts
+++ mybatis-plus/multitenant/partition/build.gradle.kts
@@ -6,6 +6,8 @@ group = "org.example"
 version = "1.0.0-SNAPSHOT"
 
 dependencies {
+    implementation("com.baomidou:mybatis-plus-spring-boot3-starter:3.5.5")
+    runtimeOnly("com.h2database:h2")
 }
 
 tasks.test {
---
+++ mybatis-plus/multitenant/partition/src/main/java/org/example/partition/UserMapper.java
@@ -0,0 +1,6 @@
+package org.example.partition;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+}
