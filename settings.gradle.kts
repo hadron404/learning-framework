d385cc175c07fa463831169e21a28c6ac460e8ce
zhouqiang <2902511578@qq.com>
2024-02-02 16:44:07 +0800

    搭建项目骨架：基于spring以及mybatis-plus的多租户数据库隔离-共享表模型方案实现

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "learning-framework"
+include("mybatis-plus")
+include("mybatis-plus:multitenant")
+include("mybatis-plus:multitenant:partition")
+findProject(":mybatis-plus:multitenant:partition")?.name = "partition"
